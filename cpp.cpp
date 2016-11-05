67eb14805fc19210999dac78dbd838babdd5bf65
Dmitry Ponyatov <dponyatov@gmail.com>
2016-11-05 13:00:40 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ cpp.cpp
@@ -0,0 +1,8 @@
+#include "hpp.hpp"
+#define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
+void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
+int main() { return yyparse(); }
+
+map<string,Sym*> glob;
+void glob_init() {}
+
---
+++ meta.hpp
@@ -0,0 +1,7 @@
+#define MODULE	"himl"
+#define TITLE	"[h]omo[i]conic [m]eta[l]anguage"
+#define ABOUT	""
+#define LICENSE	"GNU LesserGPL"
+#define AUTHOR	"Dmitry Ponyatov <dponyatov@gmail.com>"
+#define GITHUB	"https://github.com/ponyatov/" MODULE
+
