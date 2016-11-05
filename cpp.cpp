90966f4c2fdee955e614543e6b2545ccbfae348c
Dmitry Ponyatov <dponyatov@gmail.com>
2016-11-05 16:40:24 +0400

    + [] {} Sym.dump 'string' glob[].lookup scalar vector

--- cpp.cpp
+++ cpp.cpp
@@ -1,7 +1,33 @@
 #include "hpp.hpp"
 #define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
 void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
-int main() { return yyparse(); }
+int main() { glob_init(); return yyparse(); }
+
+Sym::Sym(string V) { val=V; }
+void Sym::push(Sym*o) { nest.push_back(o); }
+
+string Sym::head() { ostringstream os; os<<val<<" @"<<this;
+	return os.str(); }
+string Sym::pad(int n) { string S; for (int i=0;i<n;i++) S+='\t'; return S; }
+string Sym::dump(int depth) { string S = "\n"+pad(depth)+head();
+	if (nest.size()) S += " {";
+	for (auto it=nest.begin(),e=nest.end();it!=e;it++)
+		S += (*it)->dump(depth+1);
+	if (nest.size()) S += "\n"+pad(depth)+"}";
+	return S; }
+
+Str::Str(string V):Sym(V){}
+string Str::head() { ostringstream os; os<<"'";
+	for (int i=0;i<val.length();i++) switch (val[i]) {
+		case '\r':	os<<"\\r"; break;
+		case '\n':	os<<"\\n"; break;
+		case '\t':	os<<"\\t"; break;
+		default:	os<<val[i];
+	}
+	os<<"' @"<<this;
+	return os.str(); }
+
+Vector::Vector():Sym("[]") {}
 
 map<string,Sym*> glob;
 void glob_init() {}
--- hpp.hpp
+++ hpp.hpp
@@ -2,20 +2,38 @@
 #define _H_HPP
 
 #include <iostream>
+#include <sstream>
 #include <cstdlib>
 #include <vector>
 #include <map>
 using namespace std;
 
 struct Sym {
+	string val;
+	Sym(string);
+	vector<Sym*> nest; void push(Sym*);
+	virtual string dump(int=0); virtual string head(); string pad(int);
 };
 extern map<string,Sym*> glob;
 extern void glob_init();
 
+struct Str:Sym { Str(string); string head(); };
+
+struct Vector:Sym { Vector(); };
+
 extern int yylex();
 extern int yylineno;
 extern char* yytext;
-#define TOC(C,X) { yylval.o = new C(yytext); return X; }
+
+#define TOC(C,X) { \
+	if (glob[yytext]) { yylval.o = glob[yytext]; } \
+	else { yylval.o = new C(yytext); glob[yytext] = yylval.o; } \
+	return X; }
+#define TOS(C,X) { \
+	if (glob[LexString]) { yylval.o = glob[LexString]; } \
+	else { yylval.o = new C(LexString); glob[LexString] = yylval.o; } \
+	return X; }
+
 extern int yyparse();
 extern void yyerror(string);
 #include "ypp.tab.hpp"
